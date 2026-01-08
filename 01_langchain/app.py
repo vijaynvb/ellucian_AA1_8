46086fc03b5aa637a46de0ef0d529948dc33470e
vijaynvb <vijay.nvb@gmail.com>
2026-01-08 14:17:17 +0530

    Remove Streamlit app implementation and update Day-2 notes with Langchain details

--- 01_langchain/app.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-prompt = st.chat_input("Say something")
-if prompt:
-    with st.chat_message("User"):
-      st.write(f"User has sent the following prompt: {prompt}")
-            
